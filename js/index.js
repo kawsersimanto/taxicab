55b62d063f5eeeffba72555d173169467badeedc
simu6 <semantos678@gmail.com>
2022-09-07 22:04:21 +0600

    added preloader

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// preloader
+$(window).load(function () {
+  $(".loader").delay(2000).fadeOut("slow");
+  $("#overlayer").delay(2000).fadeOut("slow");
+});
+
 // counter js
 $(".counter").counterUp({
   delay: 10,
